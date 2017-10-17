e9c2fea0bf8b09d6a34dbb924a38821232b706c3
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-17 20:39:05 +0530

    Added Manifest and java files
    Java files include changes in main activity, intro activity and sample
    slide activity

---
+++ app/src/main/java/com/example/android/quakereport/IntroActivity.java
@@ -0,0 +1,67 @@
+package com.example.android.quakereport;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+/**
+ * Created by kvaib on 17-10-2017.
+ */
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Note here that we DO NOT use setContentView();
+
+        // Add your slide fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        addSlide(SampleSlide.newInstance(R.layout.slide_1));
+        addSlide(SampleSlide.newInstance(R.layout.slide_2));
+        addSlide(SampleSlide.newInstance(R.layout.slide_3));
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+        addSlide(AppIntroFragment.newInstance("Welcome", "This is a description", R.drawable.magnitude_circle, getColor(R.color.colorAccent)));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}
