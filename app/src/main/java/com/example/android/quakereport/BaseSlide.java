3d917196966b27387235ab58579067304d6eafdc
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-21 00:03:25 +0530

    Chnaged import statement
    Fragment

--- app/src/main/java/com/example/android/quakereport/BaseSlide.java
+++ app/src/main/java/com/example/android/quakereport/BaseSlide.java
@@ -1,11 +1,12 @@
-package com.example.android.quakereport;
+dpackage com.example.android.quakereport;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by kvaib on 20-10-2017.
