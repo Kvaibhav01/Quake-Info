e017150ffd89d61e586d8c15b5b1662aa31a19aa
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-20 19:21:19 +0530

    Added BaseSlide java file
    Replaces SampleSlide

---
+++ app/src/main/java/com/example/android/quakereport/BaseSlide.java
@@ -0,0 +1,43 @@
+package com.example.android.quakereport;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kvaib on 20-10-2017.
+ */
+
+public class BaseSlide extends Fragment {
+
+    private static final String ARG_LAYOUT_RES_ID = "layoutResId";
+    private int layoutResId;
+
+    public static BaseSlide newInstance(int layoutResId) {
+        BaseSlide baseSlide = new BaseSlide();
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
+        baseSlide.setArguments(args);
+
+        return baseSlide;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID))
+            layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(layoutResId, container, false);
+    }
+}
