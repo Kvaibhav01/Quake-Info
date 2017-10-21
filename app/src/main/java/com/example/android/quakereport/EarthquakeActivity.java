92a4be3b00b181d67b2a78cc6e217522b4cecb13
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-22 00:46:26 +0530

    Added About activity code in java and Strings file
    First commint about to be chnaged.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -59,6 +59,13 @@ public class EarthquakeActivity extends AppCompatActivity
      */
     private static final int EARTHQUAKE_LOADER_ID = 1;
 
+    private static final String TWITTER_USER_NAME = "@vaibhav_khulbe";
+    private static final String WEB_HOME_PAGE = "https://about.me/vaibhav_khulbe";
+    private static final String APP_PUBLISHER = "https://play.google.com/store/apps/developer?id=Vaibhav%20Khulbe&hl=en";
+    private static final String EMAIL_ADDRESS = "khulbevaibhavdev@gmail.com" ;
+    private static final String EMAIL_SUBJECT = "Quake App acknowledgements and issues";
+    private static final String EMAIL_BODY = "Please explain your experience with this app here...";
+
     /** Adapter for the list of earthquakes */
     private EarthquakeAdapter mAdapter;
 
@@ -112,7 +119,51 @@ public class EarthquakeActivity extends AppCompatActivity
         // Start the thread
         t.start();
 
-    
+        /* Create About activity */
+        AboutConfig aboutConfig = AboutConfig.getInstance();
+        aboutConfig.appName = getString(R.string.app_name);
+        aboutConfig.appIcon = R.mipmap.ic_launcher;
+        aboutConfig.version = "1.0.0";
+        aboutConfig.author = "Tolstoy";
+        aboutConfig.aboutLabelTitle = "About App";
+        aboutConfig.packageName = getApplicationContext().getPackageName();
+
+
+        aboutConfig.twitterUserName = TWITTER_USER_NAME;
+        aboutConfig.webHomePage = WEB_HOME_PAGE;
+
+        // app publisher for "Try Other Apps" item
+        aboutConfig.appPublisher = APP_PUBLISHER;
+
+        aboutConfig.dialog = new IDialog() {
+            @Override
+            public void open(AppCompatActivity appCompatActivity, String url, String tag) {
+                // handle custom implementations of WebView. It will be called when user click to web items. (Example: "Privacy", "Acknowledgments" and "About")
+            }
+        };
+
+        aboutConfig.analytics = new IAnalytic() {
+            @Override
+            public void logUiEvent(String s, String s1) {
+                // handle log events.
+            }
+
+            @Override
+            public void logException(Exception e, boolean b) {
+                // handle exception events.
+            }
+        };
+        // set it only if aboutConfig.analytics is defined.
+        aboutConfig.logUiEventName = "Log";
+
+        // Contact Support email details
+        aboutConfig.emailAddress = EMAIL_ADDRESS;
+        aboutConfig.emailSubject = EMAIL_SUBJECT;
+        aboutConfig.emailBody = EMAIL_BODY;
+
+
+        aboutConfig.shareMessage = getString(R.string.share_message);
+        aboutConfig.sharingTitle = getString(R.string.sharing_title);
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
