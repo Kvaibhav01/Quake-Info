7f5c2fbc651b764d90323557988b0d4c2a8f0e88
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-27 00:36:06 +0530

    Added  material-intro-screen code
    Changed library from AppIntro to material-intro-screen implementing
    first run

--- app/src/main/java/com/example/android/quakereport/IntroActivity.java
+++ app/src/main/java/com/example/android/quakereport/IntroActivity.java
@@ -5,75 +5,79 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-import com.github.paolorotolo.appintro.AppIntro;
-import com.github.paolorotolo.appintro.AppIntroFragment;
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.MessageButtonBehaviour;
+import agency.tango.materialintroscreen.SlideFragmentBuilder;
 
 /**
  * Created by kvaib on 17-10-2017.
  */
 
-public class IntroActivity extends AppIntro {
+public class IntroActivity extends MaterialIntroActivity {
 
-    @Override
+   @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Note here that we DO NOT use setContentView();
 
-        // Add your slide fragments here.
-        // AppIntro will automatically generate the dots indicator and buttons.
-        addSlide(BaseSlide.newInstance(R.layout.slide_1));
-        addSlide(BaseSlide.newInstance(R.layout.slide_2));
-        addSlide(BaseSlide.newInstance(R.layout.slide_3));
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.colorPrimary)
+                        .buttonsColor(R.color.colorAccent)
+                        .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                        .title("Welcome!")
+                        .description("To the Quake Report app")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("The app is in continuous evolvement");
+                    }
+                }, "What's good?"));
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-        addSlide(AppIntroFragment.newInstance("Welcome", "This is a description", R.drawable.magnitude_circle, getColor(R.color.colorAccent)));
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.colorPrimary)
+                        .buttonsColor(R.color.colorAccent)
+                        .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                        .title("Accurate and precise")
+                        .description("No data error in these earthquake lists")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("Straight from the USGS servers.");
+                    }
+                }, "Data source?")
 
-        // OPTIONAL METHODS
-        // Override bar/separator color.
-        setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));
 
-        // Hide Skip/Done button.
-        showSkipButton(false);
-        setProgressButtonEnabled(false);
+        );
 
-        // Turn vibration on and set intensity.
-        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
-        setVibrate(true);
-        setVibrateIntensity(30);
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.colorPrimary)
+                        .buttonsColor(R.color.colorAccent)
+                        .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                        .title("Dive in!")
+                        .description("Checkout the earthquake details with care and inform your friends and community!")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("We donot joke :')");
+                    }
+                }, "Nothing here")
+
+
+        );
+
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.colorPrimary)
+                        .buttonsColor(R.color.colorAccent)
+                        .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                        .title("Big hug!")
+                        .description("Thank you for following up the basics of the app")
+                        .build()
+
+
+        );
+        enableLastSlideAlphaExitTransition(true);
     }
-
-    @Override
-    public void onSkipPressed(Fragment currentFragment) {
-        super.onSkipPressed(currentFragment);
-        // Do something when users tap on Skip button.
-        finish();
-    }
-
-    @Override
-    public void onDonePressed(Fragment currentFragment) {
-        super.onDonePressed(currentFragment);
-        // Do something when users tap on Done button.
-        finish();
-    }
-
-    @Override
-    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
-        super.onSlideChanged(oldFragment, newFragment);
-        // Do something when the slide changes.
-    }
-    
-    private void loadMainActivity()
-    {
-        Intent intent = new Intent(this, EarthquakeActivity.class);
-        startActivity(intent);
-    }
-
-    public void getStarted(View view)
-    {
-        loadMainActivity();
-    }
-
 }
