dda4fe7b9f7e89284d9699e27a571b4646611ea3
Vaibhav Khulbe <kvaibhav01@live.in>
2017-11-04 01:08:21 +0530

    Added intent for about acitivity

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -285,6 +285,12 @@ public class EarthquakeActivity extends AppCompatActivity
             startActivity(settingsIntent);
             return true;
         }
+
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
